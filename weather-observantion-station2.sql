b9331bba0a42dca28e6f168ca64ff23c09ca4ef5
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-09-22 09:12:14 +0530

    Create weather-observantion-station2.sql

---
+++ weather-observantion-station2.sql
@@ -0,0 +1,8 @@
+/*
+QUESTION - 
+https://www.hackerrank.com/challenges/weather-observation-station-2/problem?isFullScreen=true
+
+
+CODE - 
+*/
+SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION;
