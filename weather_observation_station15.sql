4e966bdd39e18ba915058bcc4f41615953c3c60d
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-09-23 22:30:52 +0530

    Create weather_observation_station15.sql

---
+++ weather_observation_station15.sql
@@ -0,0 +1,13 @@
+/*
+QUESTION - 
+https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
+
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to  decimal places.
+
+CODE - 
+*/
+SELECT ROUND(LONG_W,4) FROM STATION
+WHERE LAT_N = (
+SELECT MAX(LAT_N) FROM STATION
+    WHERE LAT_N < 137.2345
+);
