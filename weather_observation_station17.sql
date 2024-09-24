bfb420382d45838e7999e8f5628371854179a6bf
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-09-24 22:23:13 +0530

    Create weather_observation_station17.sql

---
+++ weather_observation_station17.sql
@@ -0,0 +1,10 @@
+/*
+QUESTION - 
+https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
+
+CODE - 
+*/
+SELECT ROUND(LONG_W,4) FROM STATION
+WHERE LAT_N > 38.7780
+ORDER BY LAT_N
+LIMIT 1;
