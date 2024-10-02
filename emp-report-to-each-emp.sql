612b99d13627ced5980c58015620935a9ed48a90
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-10-02 23:18:40 +0530

    Update emp-report-to-each-emp.sql

--- emp-report-to-each-emp.sql
+++ emp-report-to-each-emp.sql
@@ -13,3 +13,15 @@ INNER JOIN Employees e2
 ON e1.employee_id = e2.reports_to
 GROUP BY e1.employee_id
 ORDER BY e1.employee_id
+
+
+OR
+
+SELECT e.employee_id,
+    e.name, 
+    COUNT(r.reports_to) as reports_count, 
+    ROUND(AVG(r.age),0) AS average_age
+FROM Employees e
+JOIN Employees r ON e.employee_id = r.reports_to
+GROUP BY e.employee_id, e.name
+ORDER BY e.employee_id;
