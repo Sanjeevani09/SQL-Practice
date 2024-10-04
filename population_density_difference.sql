95f07beb3b651b7670a46c14865f289c6ca4af52
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-10-04 23:40:34 +0530

    Create population_density_difference.sql

---
+++ population_density_difference.sql
@@ -0,0 +1,10 @@
+/*
+QUESTION - 
+
+https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+
+Query the difference between the maximum and minimum populations in CITY.
+
+CODE - 
+*/
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY;
