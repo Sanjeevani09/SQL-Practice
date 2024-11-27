4ffc4fa03df8fe1bcf8d92a04722fb0b4ef4cf54
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-11-27 19:22:28 +0530

    Create weather_observation_station_1.sql

---
+++ weather_observation_station_1.sql
@@ -0,0 +1,12 @@
+/*
+QUESTION - 
+
+https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+
+Query a list of CITY and STATE from the STATION table.
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+
+CODE - 
+*/
+SELECT CITY, STATE FROM STATION;
